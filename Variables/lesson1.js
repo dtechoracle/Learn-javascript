1746d3f3a4494e1ad60ea89387ab0831cab3e59c
dtechoracle <iamezekieljeremiah@gmail.com>
2025-01-11 22:17:00 +0100

    First Lesson: Variable declaration types

---
+++ Variables/lesson1.js
@@ -0,0 +1,22 @@
+// This lesson will cover how to declare variables in Javascript
+
+// Using let: Can be reassigned
+
+let user = "Jeremiah";
+console.log(user);
+// Output: Jeremiah
+
+user = "Bob";
+console.log(user);
+// Output: Bob
+
+// Using const: Cannot be reassigned
+const age = 21;
+console.log(age);
+// Output: 21
+// If we try to reassign age to another value, it will throw an error
+
+// Using var: function-scoped
+var city = "Lagos";
+console.log(city);
+// Output: Lagos
