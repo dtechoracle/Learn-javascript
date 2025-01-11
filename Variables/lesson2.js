fc98ae1bebd31ed7a2a8494827e28fe9d1156421
dtechoracle <iamezekieljeremiah@gmail.com>
2025-01-11 22:25:42 +0100

    Second Lesson: Naming rules for variables

---
+++ Variables/lesson2.js
@@ -0,0 +1,14 @@
+// Naming rules for Variables
+
+// 1. Variables must start with a letter, _, or $
+// 2. They cannot contain spaces or start with a Number
+// 3.Use camelCase for naming
+
+let firstName = "Jeremiah";
+let _age = 30;
+let $price = 100.41;
+// The Above declarations are all valid
+
+// let 1stPlace = "Jammie";
+// let my-name = "Matthew";
+// The Above declarations are invalid
